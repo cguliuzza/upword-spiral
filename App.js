c483b2279b4db61f2bc50b1c6c4f98239a1db18b
christinaguliuzza <christina.guliuzza@gmail.com>
2021-11-03 12:30:31 -0700

    example successfully connected rails to react

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Welcome to the Upward Spiral App</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the Upward Spiral App</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Welcome to the Upward Spiral App</Text>
+        <Text>Page Count: {count}</Text>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
@@ -19,3 +47,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
