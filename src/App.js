2b92fac6ac7f74f9a875ca66921591562a92907d
christinaguliuzza <christina.guliuzza@gmail.com>
2021-11-10 13:48:38 -0800

    back to basics with react native navigation

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
