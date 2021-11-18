b9e5b38a515e8d4b86cce74c19658193bcdaf79b
christinaguliuzza <christina.guliuzza@gmail.com>
2021-11-18 03:05:47 -0800

    timer placeholder

--- src/screens/ActionScreen/ShowActionScreen/ShowActionScreen.js
+++ src/screens/ActionScreen/ShowActionScreen/ShowActionScreen.js
@@ -3,21 +3,19 @@ import { useNavigation } from '@react-navigation/native'
 import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import CustomButton from '../../../components/CustomButton/CustomButton'
 
-const ShowActionScreen = ( key ) => {
+const ShowActionScreen = () => {
     const navigation = useNavigation();
 
-    console.log(key)
-
-    const onHomePressed = () => {
-        navigation.navigate('Home');
+    const onStartPressed = () => {
+        console.warn('The countdown begins!');
     }
 
     return (
         <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.root}>
-            <Text style={{ fontSize: 35, alignItems: 'center' }}>TEXT</Text>
+            <Text style={{ fontSize: 35, alignItems: 'center' }}>5:00</Text>
 
-            <CustomButton text='Home' onPress={onHomePressed} type='PRIMARY' />
+            <CustomButton text='Start' onPress={onStartPressed} type='PRIMARY' />
         </View>
         </ScrollView>
     )
