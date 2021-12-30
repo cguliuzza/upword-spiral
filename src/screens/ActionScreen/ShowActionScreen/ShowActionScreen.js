745c86bc44130bb3e8b33ecafaec678195431b51
christinaguliuzza <christina.guliuzza@gmail.com>
2021-12-29 19:00:25 -0800

    timer countdown

--- src/screens/ActionScreen/ShowActionScreen/ShowActionScreen.js
+++ src/screens/ActionScreen/ShowActionScreen/ShowActionScreen.js
@@ -1,32 +1,77 @@
-import React from 'react'
+import React, { useRef, useEffect, useState, useCallback } from "react";
+// import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import CustomButton from '../../../components/CustomButton/CustomButton'
 
 const ShowActionScreen = () => {
-    const navigation = useNavigation();
+    const timerIdRef = useRef(10);
+    const [counter, setCounter] = useState(10);
+    
+    const startCounter = useCallback(() => {
+      if (timerIdRef.current !== 10) {
+        return;
+      }
+      timerIdRef.current = setInterval(() => {
+        setCounter((counter) => counter - 1);
+      }, 1000);
+    }, []);
+    console.log("hello");
+    useEffect(() => {
+      console.log(timerIdRef.current);
+      if (counter > 9) {
+        startCounter();
+      }
+      return function cleanUp() {
+        if (counter < 2) {
+          clearInterval(timerIdRef.current);
+        }
+      };
+    });
+//     const navigation = useNavigation();
 
-    const onStartPressed = () => {
-        console.warn('The countdown begins!');
-    }
+//     const onStartPressed = () => {
+//         console.warn('The countdown begins!');
+//     }
 
-    return (
-        <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.root}>
-            <Text style={{ fontSize: 35, alignItems: 'center' }}>5:00</Text>
+//     return (
+//         <ScrollView showsVerticalScrollIndicator={false}>
+//         <View style={styles.root}>
+//             <Text style={{ fontSize: 35, alignItems: 'center' }}>5:00</Text>
+//             <CustomButton text='Start' onPress={onStartPressed} type='PRIMARY' />
+//         </View>
+//         </ScrollView>
+//     )
 
+
+return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+            <Text style={{ color: "teal", alignItems: 'center', fontSize: 35 }}>
+                5 Minute Moment Timer
+            </Text>
+            <Text style={{ color: "teal", alignItems: 'center', fontSize: 20 }}>
+                {counter} minutes and blank seconds
+            </Text>
             <CustomButton text='Start' onPress={onStartPressed} type='PRIMARY' />
         </View>
-        </ScrollView>
-    )
+    </ScrollView>
+);
+
 }
 
-const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center',
-        padding: 20,
-        paddingTop: 45
-    },
-})
+// const styles = StyleSheet.create({
+//     root: {
+//         alignItems: 'center',
+//         padding: 20,
+//         paddingTop: 45
+//     },
+// })
 
 export default ShowActionScreen;
+
+
+// the counter problem
+
+function RedirectUser() {
+
