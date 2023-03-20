23d81e61e36d8a515dd10f1417d76d309c5b0153
Tina Guliuzza <christina.guliuzza@gmail.com>
2023-03-20 16:34:45 -0700

    update dependencies for react-native and expo

--- README.md
+++ README.md
@@ -28,3 +28,17 @@ Dark Gray #363739
 3. Run the following command in your terminal: `npx expo start`
 
 #### Mobile: Download the Expo Go application on your iPhone from the App Store
+
+npm install -g expo-cli
+
+nvm install 16
+nvm use 16
+npm start
+This command requires Expo CLI. Do you want to install it globally [Y/n]? y
+
+npx expo login
+
+update react-native version in package.json
+expo install
+npx expo start
+Run the ios simulator
--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
 import Navigation from './navigation';
 
 const App = () => {
   return (
-    <View style={styles.root}>
-      <Navigation />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.container}>
+        <Navigation />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: '#fafafa'
-  }
-})
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+    marginVertical: 0,
+    marginHorizontal: 0,
+  },
+});
 
 export default App;
